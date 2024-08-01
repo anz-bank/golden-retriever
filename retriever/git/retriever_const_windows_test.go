5f494ac42e1e2f80e2ed6c5096f23c04236a0187
Arieh Schneier <15041913+AriehSchneier@users.noreply.github.com>
2024-08-01 12:41:05 +1000

    Try just changing the 1 string

--- retriever/git/retriever_const_windows_test.go
+++ retriever/git/retriever_const_windows_test.go
@@ -5,7 +5,7 @@ package git
 
 const (
 	pubRepoInitContent    = "# a-public-repo\nA public repo for modules testing\n"
-	pubRepoV1Content      = "# a-public-repo v0.0.1\nA public repo for modules testing\n"
+	pubRepoV1Content      = "# a-public-repo v0.0.1\r\nA public repo for modules testing\r\n"
 	pubRepoV2Content      = "# a-public-repo v0.0.2\nA public repo for modules testing\n"
 	pubRepoDevelopContent = "# a-public-repo-dev\nA public repo for modules testing\n"
 	pubRepoMainContent    = pubRepoV2Content
